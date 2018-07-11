2bedde107c364f9ad81b5a324c052d13cd4ebade
Kazuhiro Inari <inarinlab@gmail.com>
2018-07-11 19:47:57 +0900

    phpcs refactoring

--- src/Shell.php
+++ src/Shell.php
@@ -1,42 +1,53 @@
 <?php
 
 namespace Phulp\Plugin;
+
 use Phulp\Phulp;
 
-class Shell {
+class Shell
+{
+    private $phulp = null;
 
-  private $phulp = null;
+    public $config = [];
+    public $env = [];
+    public $argv = [];
 
-  public $config = [];
-  public $env = [];
-  public $argv = [];
+    public function __construct(Phulp $phulp = null)
+    {
+        $config = getcwd() . '/composer.json';
+        if (file_exists($config)) {
+            $json = file_get_contents($config);
+            $json = json_decode($json, true);
+            foreach ($json['config'] as $key => $value) {
+                $value = exec('echo ' . $value);
+                if (ctype_upper(str_replace('_', '', $key))) {
+                    $this->env[$key] = $value;
+                } else {
+                    $this->config[$key] = $value;
+                }
+            }
+        }
+        $this->argv = $_SERVER['argv'];
+        $this->phulp = $phulp ?: new Phulp();
+    }
 
-  public function __construct(Phulp $phulp = null) {
-    $config = getcwd() . '/composer.json';
-    if ( file_exists($config) ) {
-        $json = file_get_contents($config);
-        $json = json_decode($json, true);
-        foreach($json['config'] as $key => $value) {
-            $value = exec('echo ' . $value);
-            if( ctype_upper(str_replace('_', '', $key)) ) $this->env[$key] = $value;
-            else $this->config[$key] = $value;
+    public function exec(array $command, $async = false, callable $callback = null)
+    {
+        if (! isset($command['env'])) {
+            $command['env'] = $this->env;
+        }
+        if (! isset($command['env']['PATH'])) {
+            $command['env']['PATH'] = getenv('PATH');
+        }
+        if (isset($command['env']['BASHRC_PATH'])) {
+            $command['command'] = 'shopt -s expand_aliases;source $BASHRC_PATH;'."\n{$command['command']}";
+        }
+        $command['cwd'] = exec('echo ' . $command['cwd']);
+        if ($this->config['phulp_dry_run']) {
+            var_dump($command);
+            // var_dump($_SERVER['PHP_SELF'] );
+        } else {
+            $this->phulp->exec($command, $async, $callback);
         }
     }
-    $this->argv = $_SERVER['argv'];
-    $this->phulp = $phulp ?: new Phulp();
-  }
-
-  public function exec(array $command, $async = false, callable $callback = null) {
-    if ( ! isset($command['env']) ) $command['env'] = $this->env;
-    if ( ! isset($command['env']['PATH']) ) $command['env']['PATH'] = getenv('PATH');
-    if ( isset($command['env']['BASHRC_PATH']) ) {
-      $command['command'] = 'shopt -s expand_aliases;source $BASHRC_PATH;'."\n{$command['command']}";
-    }
-    $command['cwd'] = exec('echo ' . $command['cwd']);
-    if ( $this->config['phulp_dry_run'] ) {
-      var_dump($command);
-    } else {
-      $this->phulp->exec($command, $async, $callback);
-    }
-  }
 }
